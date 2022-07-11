1ef81c86af7edaf5270b1bebdf42966b8074fe98
DuyLe <minhduy4520@gmail.com>
2022-07-11 11:48:48 -0500

    Update intro.js

--- javascript/intro.js
+++ javascript/intro.js
@@ -3,4 +3,20 @@ let students=[];
 let color=['red','orange','yellow'];
 let stuff=[true,68,'cat',null];
 var store=new Array(6);
-pr
+console.log(store.length)
+
+//array methods
+/*
+push: add to end
+pop: remove from end
+shift: remove from start
+unshift: add to start
+concat: merge arrays
+includes: look for a value
+join: creates a string from an array
+reverse: reverse an array
+slice: copies a portion on an array
+splice: removes/replaces elements
+sort: sorts an array
+*/
+store.
