1096177e077df89767a22ab76b3feb3d45c211dd
DuyLe <minhduy4520@gmail.com>
2022-07-19 11:18:28 -0500

    Update app.js

--- event/DOM_Events_CODE/ChangeAndInputEvents/app.js
+++ event/DOM_Events_CODE/ChangeAndInputEvents/app.js
@@ -1,6 +1,8 @@
 const input = document.querySelector('input');
 const h1 = document.querySelector('h1');
 
+//change only shows when we blur/focus
+//input shows live (as we constantly change the input)
 // input.addEventListener('change', function (e) {
 //     console.log("CASKDJASKJHD")
 // })
