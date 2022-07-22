638919f6cc55f3508fd5650aa708bddef9c73711
DuyLe <minhduy4520@gmail.com>
2022-07-21 21:51:12 -0500

    callbacks

--- javascript/async/callback/app.js
+++ javascript/async/callback/app.js
@@ -1,3 +1,13 @@
-const delayedColorChange=(newColor, delay)=>{
-    set
+const delayedColorChange=(newColor, delay, doNext)=>{
+    setTimeout(()=>{
+        document.body.style.backgroundColor=newColor;
+        doNext && doNext();
+    }, delay)
 }
+delayedColorChange('red',1000,()=>{
+    delayedColorChange('orange',1000,()=>{
+        delayedColorChange('yellow',1000,()=>{
+
+        })
+    })
+})
