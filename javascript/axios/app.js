ae6b629f681dffecb76d4e4bba8ad6e7a31c9a2c
DuyLe <minhduy4520@gmail.com>
2022-07-26 13:01:32 -0500

    Update app.js

--- javascript/axios/app.js
+++ javascript/axios/app.js
@@ -1,1 +1,14 @@
-axios.get("https://swapi.dev/api/people/1")
+// axios.get("https://swapi.dev/api/people/1/")
+// .then((res)=>{
+//     console.log("response: ", res);
+// })
+// .catch((e)=>{
+//     console.log("error: ",e)
+// })
+
+const getStarWarsPerson=async(id)=>{
+    const res=await axios.get(`https://swapi.dev/api/people/${id}/`);
+    console.log(res.data);
+}
+
+getStarWarsPerson(5);
