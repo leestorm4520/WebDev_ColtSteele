bc6b558d928b65509945ead2608130dc1a6260c0
DuyLe <minhduy4520@gmail.com>
2022-07-26 12:50:00 -0500

    Update app.js

--- javascript/ajax/fetch/app.js
+++ javascript/ajax/fetch/app.js
@@ -1,13 +1,25 @@
 // https://swapi.dev/api/people/1
-fetch("https://swapi.dev/api/people/1")
-.then(res=>{
-    console.log('resolved',res);
-    return res.json(); //return a promise
-})
-.then((data)=>{
-    console.log(data);
-    return fetch("https://swapi.dev/api/people/2");
-})
-.catch(e=>{
-    console.log('error', e);
-})
+// fetch("https://swapi.dev/api/people/1")
+// .then(res=>{
+//     console.log('resolved',res);
+//     return res.json(); //return a promise
+// })
+// .then((data)=>{
+//     console.log(data);
+//     return fetch("https://swapi.dev/api/people/2");
+// })
+// .catch(e=>{
+//     console.log('error', e);
+// })
+
+const loadStarWarPeople=async()=>{
+    try{
+        const res=await fetch("https://swapi.dev/api/people/1");
+        const data=await res.json();
+        console.log(data);
+    }
+    catch(e){
+        console.log("error",e);
+    }
+};
+loadStarWarPeople();
