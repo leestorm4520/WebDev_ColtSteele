0fd19173ceee78ae5befd2f5164dcc2763ddd98d
DuyLe <minhduy4520@gmail.com>
2022-07-26 23:12:53 -0500

    Update app.js

--- javascript/tvSearch/app.js
+++ javascript/tvSearch/app.js
@@ -2,12 +2,9 @@ const form=document.querySelector('#searchForm');
 form.addEventListener('submit', async function(e){
     e.preventDefault();
     const searchTerm=form.elements.query.value;
-    console.log(1);
     const config={params:{q:searchTerm}};
     const res= await axios.get(`https://api.tvmaze.com/search/shows`, config);
-    console.log(2);
     displayShow(res.data);
-    console.log(3);
     form.elements.query.value='';
    
 })
@@ -15,7 +12,7 @@ const displayShow=(shows)=>{
     for(let result of shows){
         if(result.show.image){
             const img=document.createElement('IMG');
-            img.src=result.show.image.medimum;
+            img.src=result.show.image.medium;
             document.body.append(img);
         }
         
