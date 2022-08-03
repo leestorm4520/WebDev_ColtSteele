b8cd9d03886681cc0abd8d42cc3cc34fece39e5c
DuyLe <minhduy4520@gmail.com>
2022-08-03 09:51:39 -0500

    express

--- Express/intro/firstapp/index.js
+++ Express/intro/firstapp/index.js
@@ -19,6 +19,13 @@ app.get('/r/:subreddit/:postId',(req,res)=>{
     res.send(`<h1>Browse ${subreddit} - view ${postId}</h1>`)
 })
 
+app.get('/search',(req,res)=>{
+    const {q}=req.query;
+    if(!q) res.send('nothing send if nothing searched')
+    res.send(`Hi there! Search key is ${q}`);
+    
+})
+
 app.get('/cats',(req,res)=>{
     res.send('meow');
 
@@ -31,6 +38,6 @@ app.get('*',(req,res)=>{
     res.send('no route available')
 })
 
-app.listen(8080,()=>{
-    console.log('listening on port 8080!')
+app.listen(8000,()=>{
+    console.log('listening on port 8000!')
 })
