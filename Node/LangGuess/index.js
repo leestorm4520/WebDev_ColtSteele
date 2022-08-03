b1d131c2307517e92e5991d98aa6f266918fc3f0
DuyLe <minhduy4520@gmail.com>
2022-08-02 19:45:59 -0500

    update

--- Node/LangGuess/index.js
+++ Node/LangGuess/index.js
@@ -1,4 +1,16 @@
-import {franc} from 'franc';
-const langs=require('langs');
+import {createRequire} from 'module';
+const require=createRequire(import.meta.url);
 
-console.log(franc('Hello'));
+import {franc} from 'franc';
+var langs = require('langs');
+var colors=require('colors');
+
+const input=process.argv[2];
+const langCode=franc(input);
+if(langCode=='und') console.log("Sorry! Couldn't figure it out!");
+else{
+    const language=langs.where("3", langCode);
+    console.log(language.name.rainbow);
+}
+
+
