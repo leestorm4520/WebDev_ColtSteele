3891a0f13d05552ce8f275e8d0d4d94181f31b80
DuyLe <minhduy4520@gmail.com>
2022-08-08 18:36:47 -0500

    update

--- Rest/Demo/index.js
+++ Rest/Demo/index.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const {v4: uuid}=require('uuid');
+uuid();
 
 //built-in middleware to handle json callbacks
 app.use(express.urlencoded({extended:true}));
@@ -10,19 +12,23 @@ app.set('view engine','ejs');
 
 
 const comments=[
-    {
+    {   
+        id:uuid(),
         username:'John',
         comment:'that is boring'
     },
     {
+        id:uuid(),
         username:'Tu',
         comment:'that is not too bad'
     },
     {
+        id:uuid(),
         username:'Phan',
         comment:'Come on ! it is lit'
     },
     {
+        id:uuid(),
         username:'Jashun',
         comment:'What do you mean'
     }
@@ -38,9 +44,24 @@ app.get('/comments/new',(req,res)=>{
 })
 
 app.post('/comments',(req,res)=>{
-    const {username,comment}=req.body;
-    comments.push({username,comment});
+    const {username,comment}=req.body; //req.body: user input in web page's body
+    comments.push({username,comment, id:uuid()});
     res.redirect('/comments');
+})
+
+app.get('/comments/:id',(req,res)=>{
+    const {id}=req.params; //req.params: data extracted from url
+    const comment=comments.find(c=>c.id===id)
+    res.render('comments/show', {comment})
+
+})
+
+app.patch('/comments/:id',(req,res)=>{
+    const {id}=req.params;
+    const newCommentText=req.body.comment;
+    const comment=comments.find(c=>c.id===id);
+    foundComment.comment=newCommentText;
+    res.redirect('/comments')
 })
 
 app.listen(3000,()=>{
@@ -54,7 +75,7 @@ New     /comments/new       GET     Form to create new comment
 Create  /comments           POST    Create new comment on server
 Show    /comments/:id       GET     Details for one specific comment
 Edit    /comments/:id/edit  GET     Form to edit specific comment
-Update  /comments/:id       PATCH   Update specfic comment on server
+Update  /comments/:id       PATCH   Update specfic comment on server (partially) (put: for complete)
 Destroy /comments/:id       DELETE  Delete specific item on sever
 
 
