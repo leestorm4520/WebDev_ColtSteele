b4bb7d7cf86a5808818867c0622fb709eb15d8f4
DuyLe <minhduy4520@gmail.com>
2022-08-10 10:25:18 -0500

    update

--- database/MongooseBasics/index.js
+++ database/MongooseBasics/index.js
@@ -1,2 +1,19 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/movieApp',{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect('mongodb://localhost:27017/movieApp',{useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>{
+    console.log('Connection Open!!')
+})
+.catch(err=>{
+    console.log('Error!!!');
+    console.log(err);
+})
+
+const movieSchema=new mongoose.Schema({
+    title:String,
+    year:Number,
+    score:Number,
+    rating:String
+});
+
+const  Movie=mongoose.model('Movie',movieSchema);
+const DOFP= new Movie ({title:'X-Men:Day of Future Past', year:2017,score:9.2,rating:'PG-13'});
