06a31280e793c9d279b397cf3b42b9f320845709
DuyLe <minhduy4520@gmail.com>
2022-08-10 16:25:11 -0500

    Update product.js

--- database/MongooseBasics/product.js
+++ database/MongooseBasics/product.js
@@ -10,10 +10,42 @@ mongoose.connect('mongodb://localhost:27017/shopApp',{useNewUrlParser:true, useU
 
 const productSchema=new mongoose.Schema({
     name:{
-        type:String
+        type:String,
+        required:true,
+        maxLength:20
     },
     price:{
         type:Number,
-    }
+        required:true
+    },
+    onSale:{
+        type:Boolean,
+        default:false
+    },
+    categories:{
+        type:[String]
+    },
+    qty:{
+        online:{
+            type:Number,
+            default:0
+        },
+        inStore:{
+            type:Number,
+            default:0,
+        },
 
-})
+        }
+    }
+});
+const Product=mongoose.model('Product',productSchema);
+const bike=new Product({name:'Mountain Bike', price:599, categories:['Cycling','Outdoor']});
+ bike.save()
+ .then(data=>{
+    console.log('it works');
+    console.log(data);
+ })
+ .catch(err=>{
+    console.log('oh no');
+    console.log(err);
+ })
