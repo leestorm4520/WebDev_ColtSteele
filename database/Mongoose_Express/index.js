ac178c46503364529209bba632d4a1ec2c29b6dd
DuyLe <minhduy4520@gmail.com>
2022-08-17 23:10:21 -0500

    update

--- database/Mongoose_Express/index.js
+++ database/Mongoose_Express/index.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const methodOverride=require('method-override');
+
+
+app.use(methodOverride('_method'));
 
 const Product=require('./models/product');
 
@@ -40,6 +44,19 @@ app.get('/products/new',(req,res)=>{
 app.post('/products',async (req,res)=>{
     const newProduct=new Product(req.body);
     await newProduct.save();
+    res.redirect(`/products/show`,{product});
+})
+
+app.get('/products/:id/edit', async (req,res)=>{
+    const {id}=req.params;
+    const product=await Product.findById(id);
+    res.render('products/edit', {product})
+})
+
+app.put('/products/:id', async (req,res)=>{
+    const {id}=req.params;
+    const product=await Product.findByIdAndUpdate(id, req.body, {runValidators:true, new:true});
+    res.redirect(`/products/${product._id}`);
 })
 
 app.listen(3000,()=>{
