dfaef647802467bb0403ff83c2a535f52b23c9f2
DuyLe <minhduy4520@gmail.com>
2022-08-17 21:35:10 -0500

    update

--- database/Mongoose_Express/index.js
+++ database/Mongoose_Express/index.js
@@ -2,8 +2,11 @@ const express=require('express');
 const app=express();
 const path=require('path');
 
+const Product=require('./models/product');
+
+
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/shopApp',{useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect('mongodb://localhost:27017/farmStand',{useNewUrlParser:true, useUnifiedTopology:true})
 .then(()=>{
     console.log('Mongo Connection Open!!')
 })
@@ -14,9 +17,30 @@ mongoose.connect('mongodb://localhost:27017/shopApp',{useNewUrlParser:true, useU
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}));
 
 
+app.get('/products', async(req,res)=>{
+    const products= await Product.find({});
+    console.log(products)
+    res.render('products/index',{products})
+})
 
+app.get('/products/:id', async (req,res)=>{
+    const {id}=req.params;
+    const product=await Product.findById(id);
+    console.log(product);
+    res.render('products/show',{product})
+})
+
+app.get('/products/new',(req,res)=>{
+    res.render('products/new')
+})
+
+app.post('/products',async (req,res)=>{
+    const newProduct=new Product(req.body);
+    await newProduct.save();
+})
 
 app.listen(3000,()=>{
     console.log('Listen on 3000!');
